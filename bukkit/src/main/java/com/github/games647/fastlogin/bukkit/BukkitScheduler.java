467c4a5541317e5a3767f49cda282a28b24a430d
Tonttis <156517767+Tonttis@users.noreply.github.com>
2026-03-05 02:17:58 +0200

    Add Folia support: rewrite BukkitScheduler.java with Folia-aware scheduler

--- bukkit/src/main/java/com/github/games647/fastlogin/bukkit/BukkitScheduler.java
+++ bukkit/src/main/java/com/github/games647/fastlogin/bukkit/BukkitScheduler.java
@@ -27,22 +27,139 @@ package com.github.games647.fastlogin.bukkit;
 
 import com.github.games647.fastlogin.core.scheduler.AsyncScheduler;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.plugin.Plugin;
 import org.slf4j.Logger;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Future;
 
 public class BukkitScheduler extends AsyncScheduler {
 
+    private final Plugin plugin;
     private final Executor syncExecutor;
+    private final boolean isFolia;
 
     public BukkitScheduler(Plugin plugin, Logger logger) {
-        super(logger, command -> Bukkit.getScheduler().runTaskAsynchronously(plugin, command));
+        super(logger, command -> {
+            if (isFolia()) {
+                FoliaAccessor.runAsync(plugin, command);
+            } else {
+                Bukkit.getScheduler().runTaskAsynchronously(plugin, command);
+            }
+        });
+        this.plugin = plugin;
+        this.isFolia = isFolia();
 
-        syncExecutor = task -> Bukkit.getScheduler().runTask(plugin, task);
+        syncExecutor = this::runSync;
+    }
+
+    private static boolean isFolia() {
+        try {
+            Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
     }
 
     public Executor getSyncExecutor() {
         return syncExecutor;
+    }
+
+    public void runSync(Runnable task) {
+        if (isFolia) {
+            FoliaAccessor.runSync(plugin, task);
+        } else {
+            Bukkit.getScheduler().runTask(plugin, task);
+        }
+    }
+
+    public void runSync(Runnable task, Entity entity) {
+        if (isFolia) {
+            FoliaAccessor.runSync(plugin, task, entity);
+        } else {
+            Bukkit.getScheduler().runTask(plugin, task);
+        }
+    }
+
+    public void runTaskLater(Runnable task, long delay) {
+        if (isFolia) {
+            FoliaAccessor.runTaskLater(plugin, task, delay);
+        } else {
+            Bukkit.getScheduler().runTaskLater(plugin, task, delay);
+        }
+    }
+
+    public void runTaskLater(Runnable task, long delay, Entity entity) {
+        if (isFolia) {
+            FoliaAccessor.runTaskLater(plugin, task, delay, entity);
+        } else {
+            Bukkit.getScheduler().runTaskLater(plugin, task, delay);
+        }
+    }
+
+    public <T> Future<T> callSyncMethod(Callable<T> task) {
+        if (isFolia) {
+            return FoliaAccessor.callSyncMethod(plugin, task);
+        } else {
+            return Bukkit.getScheduler().callSyncMethod(plugin, task);
+        }
+    }
+
+    public <T> Future<T> callSyncMethod(Callable<T> task, Entity entity) {
+        if (isFolia) {
+            return FoliaAccessor.callSyncMethod(plugin, task, entity);
+        } else {
+            return Bukkit.getScheduler().callSyncMethod(plugin, task);
+        }
+    }
+
+    private static class FoliaAccessor {
+        static void runAsync(Plugin plugin, Runnable task) {
+            Bukkit.getAsyncScheduler().runNow(plugin, t -> task.run());
+        }
+
+        static void runSync(Plugin plugin, Runnable task) {
+            Bukkit.getGlobalRegionScheduler().execute(plugin, task);
+        }
+
+        static void runSync(Plugin plugin, Runnable task, Entity entity) {
+            entity.getScheduler().run(plugin, t -> task.run(), null);
+        }
+
+        static void runTaskLater(Plugin plugin, Runnable task, long delay) {
+            Bukkit.getGlobalRegionScheduler().runDelayed(plugin, t -> task.run(), delay);
+        }
+
+        static void runTaskLater(Plugin plugin, Runnable task, long delay, Entity entity) {
+            entity.getScheduler().runDelayed(plugin, t -> task.run(), null, delay);
+        }
+
+        static <T> Future<T> callSyncMethod(Plugin plugin, Callable<T> task) {
+            CompletableFuture<T> future = new CompletableFuture<>();
+            Bukkit.getGlobalRegionScheduler().execute(plugin, () -> {
+                try {
+                    future.complete(task.call());
+                } catch (Exception e) {
+                    future.completeExceptionally(e);
+                }
+            });
+            return future;
+        }
+
+        static <T> Future<T> callSyncMethod(Plugin plugin, Callable<T> task, Entity entity) {
+            CompletableFuture<T> future = new CompletableFuture<>();
+            entity.getScheduler().run(plugin, t -> {
+                try {
+                    future.complete(task.call());
+                } catch (Exception e) {
+                    future.completeExceptionally(e);
+                }
+            }, null);
+            return future;
+        }
     }
 }
